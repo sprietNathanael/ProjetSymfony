44993d6c54e2db7d4361ac9931ade1930a6a51de
nargil07 <nargilcorp@gmail.com>
2016-03-24 13:49:13 +0100

    Ajout du formulaire pour la decision

--- web/src/GestionEtudiantBundle/Controller/DecisionController.php
+++ web/src/GestionEtudiantBundle/Controller/DecisionController.php
@@ -6,10 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DecisionController extends Controller
 {
-    public function indexAction($id_etudiant){
-        $decision = $this->getDoctrine()
-            ->getRepository('GestionEtudiantBundle:Decisionjury')
-            ->findBy(['numEtudiant'=>$id_etudiant]);
-        return $this->render('GestionEtudiantBundle:Default:Decision.html.twig', array("decision"=>$decision));
+    public function indexAction($annee, $semestre, $id_etudiant){
+        $repository = $this->getDoctrine()
+            ->getRepository('GestionEtudiantBundle:Decisionjury');
+        $query = $repository->createQueryBuilder('d')
+            ->where('d.numEtudiant = :numEtu')
+            ->andWhere('d.semestre = :semestre')
+            ->andWhere('d.annee = :annee')
+            ->setParameter('numEtu', $id_etudiant)
+            ->setParameter('semestre', $semestre)
+            ->setParameter('annee', $annee)
+            ->getQuery();
+        $product = $query->setMaxResults(1)->getOneOrNullResult();
+        return $this->render('GestionEtudiantBundle:Default:decision.html.twig', array("decision"=>$product));
     }
 }
